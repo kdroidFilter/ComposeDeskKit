0a7ad0edede5e5da48dd9cbf72110cd9e6254af5
Elie Gambache <elyahou.hadass@gmail.com>
2026-02-20 06:05:09 +0200

    feat(http): add native-http, native-http-okhttp and native-http-ktor modules
    Pre-configured HTTP client factories that leverage NativeTrustManager
    from native-ssl to combine JVM + OS certificates out of the box.

---
+++ native-http/src/main/kotlin/io/github/kdroidfilter/nucleus/nativehttp/NativeHttpClient.kt
@@ -0,0 +1,21 @@
+package io.github.kdroidfilter.nucleus.nativehttp
+
+import io.github.kdroidfilter.nucleus.nativessl.NativeTrustManager
+import java.net.http.HttpClient
+import javax.net.ssl.SSLParameters
+
+object NativeHttpClient {
+    fun create(): HttpClient =
+        HttpClient
+            .newBuilder()
+            .withNativeSsl()
+            .build()
+
+    fun HttpClient.Builder.withNativeSsl(): HttpClient.Builder =
+        sslContext(NativeTrustManager.sslContext)
+            .sslParameters(
+                SSLParameters().apply {
+                    needClientAuth = false
+                },
+            )
+}
